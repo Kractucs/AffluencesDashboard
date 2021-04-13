9e7003153c50b7ea10155d92ad0390f1585f7417
Thomas Boeglin <thomasboe68@gmail.com>
2021-04-13 13:51:19 +0200

    Valid for first room

--- js/ical.js
+++ js/ical.js
@@ -1,15 +1,40 @@
 
 jQuery.get('https://reservation.affluences.com/api/reservations/ical?resource_id=23218&days=0', function(data) {
 
-    console.log(data)
     var jcalData = ICAL.parse(data);
     var vcalendar = new ICAL.Component(jcalData);
+    var name = vcalendar.getFirstPropertyValue('name');
     var vevents = vcalendar.getAllSubcomponents('vevent');
+    var start_times = []
+    var end_times = []
     vevents.forEach(vevent => {
-        var summary = vevent.getFirstPropertyValue('summary');
         var start_time = vevent.getFirstPropertyValue('dtstart')
         var end_time = vevent.getFirstPropertyValue('dtend')
-
-        console.log('Summary: ' + summary + " -> From : " + start_time + " To :" + end_time);
+        start_time = Date.parse(start_time)
+        end_time = Date.parse(end_time)
+        start_times.push(start_time)
+        end_times.push(end_time)
     });
+    $("#Salle1").calendar(
+        {
+            view:"day",
+            events_source: function(){
+                events = []
+                start_times.forEach(time =>
+                    events.push(
+                    {
+                        "title": "Réservé",
+                        "class": "event-important",
+                        "start": time, // Milliseconds
+                        "end": end_times[start_times.indexOf(time)] // Milliseconds
+                    })
+                )
+                return events}
+        });
+
+    document.getElementById("Salle1").getElementsByClassName("text-center")[0].textContent = name
+
 });
+
+
+
